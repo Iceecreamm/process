16338e64bf40425cc63adf6853f226b46d6eb388
Iceecreamm <g1209829751@foxmail.com>
2024-04-18 16:27:56 +0800

    message 16: form URL of new list by POST

--- lists/tests.py
+++ lists/tests.py
@@ -26,38 +26,13 @@ class ItemModelTest(TestCase):
 
 class HomePageTest(TestCase):
 
-    #def test_displays_all_list_items(self):
-    #    Item.objects.create(text='itemey 1')
-    #    Item.objects.create(text='itemey 2')
-    #    response = self.client.get('/')
-    #    self.assertIn('itemey 1', response.content.decode())
-    #    self.assertIn('itemey 2', response.content.decode())
-    
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response,'home.html')
-        
-    def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-    
-    def test_redirects_after_POST(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})    
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-new-page/')
-        
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
     
     def test_home_page_return_correct_html(self):
         request = HttpRequest()
-        #response = home_page(request)
         response = self.client.get('/')
-        #html = response.content.decode('utf8')
         html = response.content.decode('utf8').strip()  # 去除可能存在的尾随空白
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
@@ -76,6 +51,18 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/the-new-page/')
         
         self.assertContains(response, 'itemey 1')
-        self.assertContains(response, 'itemey 2') # (1)
+        self.assertContains(response, 'itemey 2')
+
+class NewListTest(TestCase):
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
         
-# Create your tests here.
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+    
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})    
+        self.assertRedirects(response, '/lists/the-new-page/')
+
